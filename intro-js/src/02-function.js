960b642035f19d6be1e4d3c74b16a2aea169bacb
javiperales <javiperales95@gmail.com>
2024-02-14 18:10:43 +0100

    primeros pasos react

---
+++ intro-js/src/02-function.js
@@ -0,0 +1,11 @@
+const invoice = {
+    id: 10,
+    name: 'Compras oficina',
+    date: new Date (),
+    client: 'Jhon Doe',
+    total: 1000 
+};
+
+invoice.client ="Paco mermela";
+
+console.log(invoice)
